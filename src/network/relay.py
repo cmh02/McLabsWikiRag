a6b042c452365891e87a024a60ef1852c87be22d
cmh02 <cmh0201@auburn.edu>
2026-07-04 14:18:04 -0400

    Refactoring relay flow

--- src/network/relay.py
+++ src/network/relay.py
@@ -14,7 +14,7 @@ import uuid
 import logging
 from typing import List, Dict
 
-from src.utils.enum import TicketAction
+from src.utils.enum import TicketAction, RelayDestination
 
 '''
 OUTBOUND RELAY
@@ -26,10 +26,22 @@ class MCL_RelayQueueData():
 
 	Class to manage the data associated with a queued outbound relay call.
 	'''
-	def __init__(self, ticketId: int, action: TicketAction, time: float):
-		self.ticketId = ticketId
-		self.action = action
-		self.timestamp = time
+	def __init__(self, ticketId: int, action: TicketAction, destination: RelayDestination, originTime: float, lastAttemptTime: float | None):
+		
+		# ID of the ticket that was updated
+		self.ticketId: int = ticketId
+
+		# Specific action that was taken - see enum TicketAction for possible values
+		self.action: TicketAction = action
+
+		# Destination of the update - see enum RelayDestination for possible values
+		self.destination: RelayDestination = destination
+
+		# Time the update was created (epoch time)
+		self.originTime: float = originTime
+
+		# Time the update was last attempted to be sent (epoch time)
+		self.lastAttemptTime: float | None = lastAttemptTime
 
 class MCL_OutboundRelay():
 	'''
@@ -59,12 +71,70 @@ class MCL_OutboundRelay():
 		self.logger = logging.getLogger("MCL_API_Logger")
 		self.logger.info(f"Initialized MCL Outbound Relay on PID {os.getpid()}.")
 
-		# Make two outbound queues for Minecraft server and Discord bot
-		self.queue_Minecraft: List[uuid.UUID] = []
-		self.queue_Discord: List[uuid.UUID] = []
+		# Queue Poll Interval defines how often (in seconds) we check queue for updates that should be retried
+		env_relayQueuePollInterval = os.getenv("MCL_RELAY_QUEUE_POLL_INTERVAL")
+		if not env_relayQueuePollInterval:
+			self.logger.error("MCL_RELAY_QUEUE_POLL_INTERVAL environment variable is not set.")
+			raise ValueError("MCL_RELAY_QUEUE_POLL_INTERVAL environment variable is not set.")
+		self.relayQueuePollInterval = float(env_relayQueuePollInterval)
+		if not self.relayQueuePollInterval:
+			self.logger.error("MCL_RELAY_QUEUE_POLL_INTERVAL environment variable could not be parsed correctly.")
+			raise ValueError("MCL_RELAY_QUEUE_POLL_INTERVAL environment variable could not be parsed correctly.")
+		if self.relayQueuePollInterval <= 0:
+			self.logger.error("MCL_RELAY_QUEUE_POLL_INTERVAL environment variable must be a positive number.")
+			raise ValueError("MCL_RELAY_QUEUE_POLL_INTERVAL environment variable must be a positive number.")
+		
+		# Queue Retry Interval defines how long (in seconds) we wait before retrying an update that has not been ACK'd
+		env_relayQueueRetryInterval = os.getenv("MCL_RELAY_QUEUE_RETRY_INTERVAL")
+		if not env_relayQueueRetryInterval:
+			self.logger.error("MCL_RELAY_QUEUE_RETRY_INTERVAL environment variable is not set.")
+			raise ValueError("MCL_RELAY_QUEUE_RETRY_INTERVAL environment variable is not set.")
+		self.relayQueueRetryInterval = float(env_relayQueueRetryInterval)
+		if not self.relayQueueRetryInterval:
+			self.logger.error("MCL_RELAY_QUEUE_RETRY_INTERVAL environment variable could not be parsed correctly.")
+			raise ValueError("MCL_RELAY_QUEUE_RETRY_INTERVAL environment variable could not be parsed correctly.")
+		if self.relayQueueRetryInterval <= 0:
+			self.logger.error("MCL_RELAY_QUEUE_RETRY_INTERVAL environment variable must be a positive number.")
+			raise ValueError("MCL_RELAY_QUEUE_RETRY_INTERVAL environment variable must be a positive number.")
+
+		# Relay outbound queue
+		self.queue: List[uuid.UUID] = []
 
 		# Dictionaries for update information
 		self.data: Dict[uuid.UUID, MCL_RelayQueueData] = {}
+
+	def relay(self, ticketId: int, action: TicketAction):
+		'''
+		# Master Relay
+
+		Notifies all relevant external systems (Minecraft server, Discord bot) of a ticket update.
+
+		## Parameters
+		- `ticketId` (int): The ID of the ticket that was updated.
+		- `action` (TicketAction): The action that was taken on the ticket.
+		'''
+
+		# Create update for minecraft
+		updateId_Minecraft: uuid.UUID = uuid.uuid4()
+		self.data[updateId_Minecraft] = MCL_RelayQueueData(
+			ticketId = ticketId, 
+			action = action, 
+			destination = RelayDestination.MINECRAFT,
+			originTime = time.time(),
+			lastAttemptTime = None
+		)
+		self.queue.append(updateId_Minecraft)
+
+		# Create update for discord
+		updateId_Discord: uuid.UUID = uuid.uuid4()
+		self.data[updateId_Discord] = MCL_RelayQueueData(
+			ticketId = ticketId, 
+			action = action, 
+			destination = RelayDestination.DISCORD,
+			originTime = time.time(),
+			lastAttemptTime = None
+		)
+		self.queue.append(updateId_Discord)
 
 	def acknowledgeUpdate(self, updateId: uuid.UUID):
 		'''
@@ -75,73 +145,9 @@ class MCL_OutboundRelay():
 		## Parameters
 		- `updateId` (uuid.UUID): The unique ID of the update to acknowledge.
 		'''
-		if updateId in self.queue_Minecraft:
-			self.queue_Minecraft.remove(updateId)
-			self.data.pop(updateId, None)
-			self.logger.info(f"Acknowledged update {updateId} from Minecraft server and removed from queue.")
-		elif updateId in self.queue_Discord:
-			self.queue_Discord.remove(updateId)
-			self.data.pop(updateId, None)
-			self.logger.info(f"Acknowledged update {updateId} from Discord bot and removed from queue.")
-		else:
+		if updateId not in self.data:
 			self.logger.warning(f"Attempted to acknowledge unknown update {updateId}.")
-
-	def notifyAll(self, ticketId: int, action: TicketAction):
-		'''
-		# Notify All
-
-		Notifies all relevant external systems (Minecraft server, Discord bot) of a ticket update.
-
-		## Parameters
-		- `ticketId` (int): The ID of the ticket that was updated.
-		- `action` (TicketAction): The action that was taken on the ticket.
-		'''
-
-		self.notifyMinecraftServer(ticketId, action)
-		self.notifyDiscordBot(ticketId, action)
-
-	def notifyMinecraftServer(self, ticketId: int, action: TicketAction):
-		'''
-		# Notify Minecraft Server
-
-		Notifies the Minecraft server of a ticket update via an outbound API call.
-
-		## Parameters
-		- `ticketId` (int): The ID of the ticket that was updated.
-		- `action` (TicketAction): The action that was taken on the ticket.
-		'''
-
-		# Log the outbound API call
-		logger = logging.getLogger("MCL_API_Logger")
-		logger.info(f"Notifying Minecraft server of ticket {ticketId} update with action {action.value}.")
-
-		# Generate a unique update ID for queueing / tracking
-		updateId: uuid.UUID = uuid.uuid4()
-		self.data[updateId] = MCL_RelayQueueData(
-			ticketId = ticketId, 
-			action = action, 
-			time = time.time()
-		)
-
-	def notifyDiscordBot(self, ticketId: int, action: TicketAction):
-		'''
-		# Notify Discord Bot
-
-		Notifies the Discord bot of a ticket update via an outbound API call.
-
-		## Parameters
-		- `ticketId` (int): The ID of the ticket that was updated.
-		- `action` (TicketAction): The action that was taken on the ticket.
-		'''
-
-		# Log the outbound API call
-		logger = logging.getLogger("MCL_API_Logger")
-		logger.info(f"Notifying Discord bot of ticket {ticketId} update with action {action.value}.")
-
-		# Generate a unique update ID for queueing / tracking
-		updateId: uuid.UUID = uuid.uuid4()
-		self.data[updateId] = MCL_RelayQueueData(
-			ticketId = ticketId, 
-			action = action, 
-			time = time.time()
-		)
+			return
+		self.queue.remove(updateId)
+		data = self.data.pop(updateId, None)
+		self.logger.info(f"Acknowledged update {updateId} and removed update from queue: {data}.")
