2b89d0026b6179025470adde8dac51ab1f3334fe
cmh02 <cmh0201@auburn.edu>
2026-07-05 19:35:29 -0400

    Resolving dependency issues in api src

--- src/api.py
+++ src/api.py
@@ -10,37 +10,21 @@ MODULE IMPORTS
 
 # System
 import os
-import asyncio
-import logging
-from datetime import datetime
 from contextlib import asynccontextmanager
 
-# Typing
-from typing import cast, Dict, Optional
-
 # API
-from pydantic import BaseModel, Field, field_validator
-from fastapi.responses import JSONResponse
-from fastapi.encoders import jsonable_encoder
-from fastapi import FastAPI, HTTPException, Request
-from slowapi.util import get_remote_address
+from fastapi import FastAPI
 from slowapi.errors import RateLimitExceeded
 from slowapi.middleware import SlowAPIMiddleware
-from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi import _rate_limit_exceeded_handler
 
 # Google API
 from google import genai
 
 # MCL Packages
-from src.rag import MCL_WikiRag
 from src.utils.logger import MCL_Logger
-from src.network.security import verifyRequest
-from src.rag.docfetch import MCL_WikiEmbedder
 from src.internal.helpmanager import MCL_HelpManager
 from src.internal.mongo import MCL_MongoManager
-from src.utils.datatypes import Message
-from src.network.schemas import BaseHelpQuestionSchema, QuestionSchema
-from src.utils.enum import TicketType, TicketStatus, TicketFeedback, UpdateSource
 from src.network.limiter import limiter
 from src.network.relay import MCL_OutboundRelay
 from src.network.endpoints import router as InternalEndpointsRouter
@@ -60,17 +44,17 @@ async def lifespan(app: FastAPI):
 	app.state.logger = MCL_Logger.setup_logger()
 
 	# Gemini client
-	if not os.getenv("GOOGLE_GEMINI_API_KEY"):
-		app.state.logger.error("GOOGLE_GEMINI_API_KEY environment variable is not set.")
-		raise ValueError("GOOGLE_GEMINI_API_KEY environment variable is not set.")
-	app.state.InstanceClient = genai.Client(api_key=os.getenv("GOOGLE_GEMINI_API_KEY"))
+	# if not os.getenv("GOOGLE_GEMINI_API_KEY"):
+	# 	app.state.logger.error("GOOGLE_GEMINI_API_KEY environment variable is not set.")
+	# 	raise ValueError("GOOGLE_GEMINI_API_KEY environment variable is not set.")
+	# app.state.InstanceClient = genai.Client(api_key=os.getenv("GOOGLE_GEMINI_API_KEY"))
 
 	# Load the index and documents
-	app.state.InstanceWikiEmbedder = MCL_WikiEmbedder(client=app.state.InstanceClient)
-	app.state.InstanceWikiEmbedder.loadIndexAndDocuments()
+	# app.state.InstanceWikiEmbedder = MCL_WikiEmbedder(client=app.state.InstanceClient)
+	# app.state.InstanceWikiEmbedder.loadIndexAndDocuments()
 
 	# RAG instance
-	app.state.InstanceRag = MCL_WikiRag(client=app.state.InstanceClient, wikiEmbedder=app.state.InstanceWikiEmbedder)
+	# app.state.InstanceRag = MCL_WikiRag(client=app.state.InstanceClient, wikiEmbedder=app.state.InstanceWikiEmbedder)
 
 	# Initialize Help Manager
 	MCL_HelpManager().initialize()
